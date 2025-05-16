103dc313b18fc407febc092cf92848e0cafe6f25
Ivan Joel More Flores <ivanyoe79@gmail.com>
2025-05-15 21:50:34 -0500

    fix: update

--- src/presentation/components/div-tittle/index.tsx
+++ src/presentation/components/div-tittle/index.tsx
@@ -11,6 +11,48 @@ const DSDivTittle = ({ title, onClick, divider = false }: IProps) => {
   return (
     <div className={styles["div-arrival"]}>
       <p className={styles["p-arrival"]}>{title}</p>
+      <div className={styles["div-cards"]}>
+        <div>
+          <div>
+            <img src="/src/presentation/assets/img/card.png" alt="Card" />
+          </div>
+          <div>
+            <p className={styles["p-title-product"]}>T-shirt with Tape Details</p>
+            <p>3.5</p>
+            <p className={styles["p-price-product"]}>$120</p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <img src="/src/presentation/assets/img/card.png" alt="Card" />
+          </div>
+          <div>
+            <p className={styles["p-title-product"]}>T-shirt with Tape Details</p>
+            <p>3.5</p>
+            <p className={styles["p-price-product"]}>$120</p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <img src="/src/presentation/assets/img/card.png" alt="Card" />
+          </div>
+          <div>
+            <p className={styles["p-title-product"]}>T-shirt with Tape Details</p>
+            <p>3.5</p>
+            <p className={styles["p-price-product"]}>$120</p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <img src="/src/presentation/assets/img/card.png" alt="Card" />
+          </div>
+          <div>
+            <p className={styles["p-title-product"]}>T-shirt with Tape Details</p>
+            <p>3.5</p>
+            <p className={styles["p-price-product"]}>$120</p>
+          </div>
+        </div>
+      </div>
       <DSButton
         color="black"
         backgroundColor="transparent"
